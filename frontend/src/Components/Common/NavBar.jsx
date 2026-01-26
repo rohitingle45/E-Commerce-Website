bdb24dc9dadcda6fbaf3650500d195cd096b2836
Rohit Ingle <rohitingle96424@gmail.com>
2026-01-26 17:42:39 +0530

    logo name change to E-App

--- frontend/src/Components/Common/NavBar.jsx
+++ frontend/src/Components/Common/NavBar.jsx
@@ -37,7 +37,7 @@ const NavBar = () => {
           {/* Left-Logo */}
           <div>
             <Link to="/" className="text-2xl font-bold">
-              MyApp
+              E-App
             </Link>
           </div>
 
